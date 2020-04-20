e3e19895ebc036b062e2f5a4b0f0aa8dc6c6d649
hengkysuryaa <hengkysurya88@gmail.com>
2020-04-20 22:53:27 +0700

    Update regexmatch.py

--- regexmatch.py
+++ regexmatch.py
@@ -1,15 +1,22 @@
 import re
 from nltk.tokenize import sent_tokenize
 
-with open('text.txt', 'r') as file:
+with open('text2.txt', 'r') as file:
     text = file.read().replace('\n', ' ')
 
 def searchangka(text):
-    angka = re.findall(r'\d+[\,\.\d]\d*\s|\d+\s', text)
+    angka = re.findall(r'\s\d+[\,\.\d]\d*\s|^\d+\s', text)
+    #'\d+[\,\.\d]\d*\s|\d+\s'
+    # ([^\d]|^)\d{1,}[,.]\d{1,}([^\d]|$) --
+    # ((\d|^)\d{0,}([^\d]|$))
+    # ((\d|^)\d{0,})
+    # (\s(\d|^)\d{0,}\s) --
+    # ^\d+
+
     return angka
 
 def searchtanggal(text):
-    tanggal = re.findall(r'Senin.+[\)B]|Selasa.+[\)B]|Rabu.+[\)B]|Kamis.+[\)B]|Jumat.+[\)B]|Sabtu.+[\)B]|Minggu.+[\)B]|\d{1,2}\s\w+\s\d+|\d{1,2}\sJan\w+|\d{1,2}\sFeb\w+|\d{1,2}\sMar\w+|\d{1,2}\sApr\w+|\d{1,2}\sMei|\d{1,2}\sJun\w+|\d{1,2}\sJul\w+|\d{1,2}\sAg\w+|\d{1,2}\sSep\w+|\d{1,2}\sOkt\w+|\d{1,2}\sNov\w+|\d{1,2}\sDes\w+|kemarin|hari ini|besok|lusa', text)
+    tanggal = re.findall(r'Senin.+[\)B]|Selasa.+[\)B]|Rabu.+[\)B]|Kamis.+[\)B]|Jumat.+[\)B]|Sabtu.+[\)B]|Minggu.+[\)B]|\d{1,2}\s\w+\s\d+|\d{1,2}\sJan\w*|\d{1,2}\sFeb\w*|\d{1,2}\sMar\w*|\d{1,2}\sApr\w*|\d{1,2}\sMei|\d{1,2}\sJun\w*|\d{1,2}\sJul\w*|\d{1,2}\sAg\w*|\d{1,2}\sSep\w*|\d{1,2}\sOkt\w*|\d{1,2}\sNov\w*|\d{1,2}\sDes\w*|kemarin|hari ini|besok|lusa', text)
     return tanggal
 
 def searchtanggalartikel():
@@ -37,4 +44,4 @@ def regexmatch(text,pattern):
                     print(tang)
             print(kal)
 
-regexmatch(text,'terkonfirmasi positif')
+regexmatch(text,'terkonfirmasi')
