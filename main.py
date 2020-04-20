bd14cbc2d9b1a2501a3028e2dda6138396c8430a
hengkysuryaa <hengkysurya88@gmail.com>
2020-04-20 20:20:34 +0700

    post input user

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/", methods=["GET","POST"])
 def home():
-    if request.method == 'POST':
-        return (request.form.get('algorithm'))
+    if request.method == "POST":
+        keyword = request.form.get("keyword")
+        algo = request.form.get("algorithm")
+        print(keyword)
+        print(algo) 
     return render_template("mainpage.html")
 
 @app.route("/about")
