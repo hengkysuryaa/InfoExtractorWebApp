1a3c93a40bed0ad01888441cc9623931d263c8e6
hengkysuryaa <hengkysurya88@gmail.com>
2020-04-21 17:41:21 +0700

    show nearest num

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from nltk.tokenize import sent_tokenize
 import kmp
 import bm
@@ -12,9 +12,14 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOADER
 app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024 # maksimum file 8 mb
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/")
 def home():
+    return render_template("mainpage.html")
+
+@app.route("/", methods=["POST"])
+def submit():
     if request.method == "POST":
+        list = []
         uploaded_files = request.files.getlist("file[]") # mengambil file yang diupload
         for berkas in uploaded_files:
             if berkas:
@@ -29,6 +34,9 @@ def home():
                     if (algo == "kmp"):
                         pos = kmp.kmpsearch(kal, keyword)
                         if (pos != -1):
+                            angka = regexmatch.searchangka(kal)
+                            posKeyword = kal.find(keyword)
+                            print(regexmatch.nearestNum(angka,posKeyword,kal))
                             print(kal, end='')
                             print('(' + berkas.filename + ')')
                     if (algo == "bm"):
@@ -39,8 +47,20 @@ def home():
                     if (algo == "regex"):
                         regexmatch.regexsearch(kal, keyword, berkas.filename, sentences)
                 os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename)) #hapus file yang diupload
-                        
-    return render_template("mainpage.html")
+        
+        tes = {
+            "Jumlah":"13",
+            "Waktu":"52",
+            "Kalimat":"haha"
+        }
+        list.append(tes)
+        tes2 = {
+            "Jumlah":"14",
+            "Waktu":"53",
+            "Kalimat" : "hehe"
+        }
+        list.append(tes2)
+        return render_template("resultpage.html",list = list)
 
 @app.route("/about")
 def about():
--- src/regexmatch.py
+++ src/regexmatch.py
@@ -36,3 +36,14 @@ def regexsearch(kal,pattern,filename,text):
                 print(tang)
         print(kal,end='')
         print('(' + filename + ')')
+
+def nearestNum(listangka,posKeyword,kal):
+    nearposAng = -1
+    nearAng = ''
+    for ang in listangka:
+        ang.replace(' ','')
+        posAng = kal.rfind(ang,0,posKeyword)
+        if (posAng > nearposAng):
+            nearAng = ang
+            nearposAng = posAng
+    return kal[nearposAng:(nearposAng+len(nearAng))].replace(' ','')
